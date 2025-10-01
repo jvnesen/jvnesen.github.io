ff42be583405b0064dae68c8e6c4519f31260932
Jonathan Liu <jonathanliu@Mac-2005.lan>
2025-09-30 20:59:45 -0700

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   images: { unoptimized: true },
+  basePath: isProd ? '/jvnesen.github.io' : '',
+  assetPrefix: isProd ? '/jvnesen.github.io/' : '',
   basePath: '', // No basePath needed for user site at root
   assetPrefix: './', // Ensures assets are served relative to the root,
 };
