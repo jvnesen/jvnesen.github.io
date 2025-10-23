85675069ae941ba14bf3adfbffd2df7e5d53c561
Jonathan Liu <jonathanliu@Mac-3873.lan>
2025-10-22 21:53:12 -0700

    a

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   images: { unoptimized: true },
-  basePath: isProd ? '/jvnesen.github.io' : '', // Use repo name as basePath in production
-  assetPrefix: isProd ? '/jvnesen.github.io/' : '', // Use repo name as assetPrefix in production
 };
 
 module.exports = nextConfig;
